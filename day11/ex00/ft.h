da6ba1f014c279e1638178e02d4c4bc242c1715e
TomLott <68801099+TomLott@users.noreply.github.com>
2020-08-03 09:06:02 +0300

    Add files via upload

---
+++ day11/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itollett <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/31 09:09:00 by itollett          #+#    #+#             */
+/*   Updated: 2020/08/01 09:39:18 by itollett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_H
+# define __FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
---
+++ day11/ex02/main.c
@@ -0,0 +1,10 @@
+#include "ft_abs.h"
+
+#include <stdio.h>
+
+#define ABS(Value) ft_abs(Value)
+int main()
+{
+    printf("%lu\n", ABS(5));
+}
+
