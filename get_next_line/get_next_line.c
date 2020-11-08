afe9ea269c8920963429abe0a29dfcffd0ef4380
TomLott <68801099+TomLott@users.noreply.github.com>
2020-11-08 19:09:03 +0300

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itollett <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/08 17:08:51 by itollett          #+#    #+#             */
+/*   Updated: 2020/11/08 19:08:47 by itollett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <stdio.h>
-void ft_clear(char *str)
-{
-    int i;
 
-    i = 0;
-    if (str[i])
-    {
-        while(str[i])
-            str[i++] = '\0';
-    }
+static void		ft_clean(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[i])
+	{
+		while(str[i])
+			str[i++] = '\0';
+	}
 }
 
-static int	ft_fulfilling(char **leftover, char **line)
+static int		ft_fulfilling(char **leftover, char **line)
 {
-	int len;
 	char *pointer;
 	char *temp;
-	
+
 	if ((pointer = ft_strchr(*leftover, '\n')))
 	{
 		*pointer++ = '\0';
@@ -29,32 +41,34 @@ static int	ft_fulfilling(char **leftover, char **line)
 	else
 	{
 		*line = ft_strdup(*leftover);
-		ft_clear(*leftover);
-		return(0);
+		ft_clean(*leftover);
+		return (0);
 	}
 	return (1);
 }
-	
 
-static int	ft_check_available(char **leftover, char **line, int available)
+static int		ft_check_available(char **leftover, char **line, int available)
 {
 	if (available < 0)
 		return (-1);
 	else if (available == 0 && !(*leftover))
+	{	
+		*line = ft_strdup("");
 		return (0);
+	}
 	else
 		return (ft_fulfilling(leftover, line));
 }
 
-int get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
-	static char *leftover[1024];
-	char buff[BUFFER_SIZE + 1];
-	int available;
-	char *temp;
-	char *pointer;
+	static char	*leftover[1024];
+	char		*buff;
+	int			available;
+	char		*temp;
 
-	if (!line && fd < 0)
+	if (!line || fd < 0 || !(buff = (char *)malloc(BUFFER_SIZE + 1))
+			|| (read(fd, buff, 0)) < 0)
 		return (-1);
 	while ((available = read(fd, buff, BUFFER_SIZE)) > 0)
 	{
@@ -70,6 +84,7 @@ int get_next_line(int fd, char **line)
 		if (ft_strchr(leftover[fd], '\n'))
 			break ;
 	}
+	free(buff);
 	return (ft_check_available(&leftover[fd], line, available));
 }
 /*
@@ -87,15 +102,13 @@ int main()
         free(line);
     }
     printf("i = %d %s &&&&&&\n", i, line);
-	free(line);
+    free(line);
     while((i = get_next_line(fd2, &line)))
     {
         printf("i = %d %s !!!!!!!!!!!!!!\n\n", i, line);
         free(line);
     }
     printf("i = %d %s &&&&&&\n", i, line);
-	free(line);
-	while (1);
+    free(line);
     return (0);
-}
-*/
+}*/
