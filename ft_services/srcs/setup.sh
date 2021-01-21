258d8ec0033c7a64c30f739eb0166b5d381dfe1c
TomLott <68801099+TomLott@users.noreply.github.com>
2021-01-21 20:54:37 +0300

    Add files via upload

--- ft_services/srcs/setup.sh
+++ ft_services/srcs/setup.sh
@@ -16,14 +16,14 @@ kubectl apply -f mysql.yaml
 docker build -t wordpress_image wordpress/
 kubectl apply -f wordpress.yaml
 
-docker build -t phpmyadmin_image phpmyadmin/
-kubectl apply -f phpmyadmin.yaml
-
 docker build -t grafana_image grafana/
 kubectl apply -f grafana.yaml
 
 docker build -t influxdb_image influxdb/
 kubectl apply -f influxdb.yaml
+
+docker build -t phpmyadmin_image phpmyadmin/
+kubectl apply -f phpmyadmin.yaml
 
 docker build -t ftps_image ftps/
 kubectl apply -f ftps.yaml
